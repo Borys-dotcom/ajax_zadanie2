d3c034cf45b5c3db0d80be39b138e548ff35fd0a
Borys-dotcom <184245222+Borys-dotcom@users.noreply.github.com>
2024-11-16 07:47:13 +0100

    solution

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
 let button = document.querySelector('#btnGetData');
 
+button.addEventListener("click", () => {
+
+    $.get('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+        .done(function(data){
+
+            let displayDiv = document.createElement('div');
+            displayDiv.id = "dane-programisty";
+            button.insertAdjacentElement("afterend", displayDiv);
+            displayDiv.innerText += data.firma + '\n';
+            displayDiv.innerText += data.imie + '\n';
+            displayDiv.innerText += data.nazwisko + '\n';
+            displayDiv.innerText += data.zawod + '\n';
+
+        })
+});
